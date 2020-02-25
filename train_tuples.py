cfc6e6156e41024749fc2f8a73a3f6a43249ae19
shahinsaleh <ssaleh@kth.se>
2020-02-25 14:13:26 +0100

    Preprocess

--- train_tuples.py
+++ train_tuples.py
@@ -6,4 +6,4 @@ class Trainer:
         self.spec2 = spec2
 
     def __str__(self):
-        print(f"mask1: {self.mask1} mask2: {self.mask2} spec1: {self.spec1} spec1: {self.spec1}")
+        print(f"mask1: {self.mask1} spec1: {self.spec1} mask2: {self.mask2} spec2: {self.spec1}")
